9ec82ed7a538a933f81d26f927b31b8a2f1cc242
Maxviz <meitstudying@gmail.com>
2024-02-24 14:51:54 +0200

    Adding add trip button in Cities list

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -3,14 +3,18 @@ import "./Cities.style.scss";
 import CityCard from "../CityCard/CityCard";
 
 const Cities = () => {
-    const cities = [1, 3, 56, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const cities = [1, 3, 56, 6, 1];
 
     return (
         <div className="cities-container">
             <div className="scroll">
-                {cities.map((city) => (
-                    <CityCard key={city.id} />
+                {cities.map((city, index) => (
+                    <CityCard key={index} city={city} />
                 ))}
+                <button className="add-city-button">
+                    <p className="plus">+</p>
+                    <p>Add trip</p>
+                </button>
             </div>
         </div>
     );
