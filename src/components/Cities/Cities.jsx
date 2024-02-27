4c3b0022ad3ac7b61305a382d15a2099345c9397
Maxviz <meitstudying@gmail.com>
2024-02-27 10:39:02 +0200

    Adding scroll buttons to slider

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -1,12 +1,15 @@
-import {useState} from "react";
+import {useState, useRef} from "react";
 import "./Cities.style.scss";
 import CityCard from "../CityCard/CityCard";
 import {useGetUserTrips} from "../../api/queries";
 import Modal from "../Modal/Modal";
+import {SlArrowRight} from "react-icons/sl";
+import {SlArrowLeft} from "react-icons/sl";
 
 const Cities = ({searchCity}) => {
     const {data: cities, isLoading, isError, error} = useGetUserTrips();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const scrollRef = useRef(null); // Ref for accessing the scroll container
 
     const handleModalOpen = () => {
         setIsModalOpen(true);
@@ -22,6 +25,26 @@ const Cities = ({searchCity}) => {
         city.cityName.toLowerCase().includes(searchCity.toLowerCase())
     );
 
+    // Function to scroll the container left
+    const scrollLeft = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: -200,
+                behavior: "smooth",
+            });
+        }
+    };
+
+    // Function to scroll the container right
+    const scrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: 200,
+                behavior: "smooth",
+            });
+        }
+    };
+
     if (isLoading) {
         return <h1>Loading</h1>;
     }
@@ -30,16 +53,22 @@ const Cities = ({searchCity}) => {
     }
     return (
         <div className="cities-container">
-            <div className="scroll">
-                {filteredAndSortedCities.map((city) => (
-                    <CityCard key={city.id} city={city} />
-                ))}
+            <button className="scroll-arrow left" onClick={scrollLeft}>
+                <SlArrowLeft />
+            </button>
+            <div className="scroll" ref={scrollRef}>
                 <button className="add-city-button" onClick={handleModalOpen}>
                     <p className="plus">+</p>
                     <p>Add trip</p>
                 </button>
+                {filteredAndSortedCities.map((city) => (
+                    <CityCard key={city.id} city={city} />
+                ))}
                 {isModalOpen && <Modal closeModal={() => setIsModalOpen(false)} />}
             </div>
+            <button className="scroll-arrow right" onClick={scrollRight}>
+                <SlArrowRight />
+            </button>
         </div>
     );
 };
