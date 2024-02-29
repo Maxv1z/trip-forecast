b9eab87ee71e9bf82734dc7dc9a9c3cc40b3af53
Maxviz <meitstudying@gmail.com>
2024-02-29 10:35:50 +0200

    changing cities import to cities.js to resolve an issue with import in prod

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import "./Modal.style.scss";
 
 import {useState, useRef} from "react";
 import {MdClose} from "react-icons/md";
-import cities from "../../assets/cities";
+import cities from "../../assets/cities.js";
 
 import {addTripToDb} from "../../api";
 import {useUserAuth} from "../../context/AuthContext";
